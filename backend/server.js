8f7a23b44914b100226afac974abc61555d7ca98
Hanguangwu <162727297+Hanguangwu@users.noreply.github.com>
2024-09-25 23:24:50 +0800

    update for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import express from 'express';
@@ -14,15 +15,26 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "*" }));
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use("/api/auth", authRoutes);
 app.use("/api", travelStoryRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectToMongoDB();
